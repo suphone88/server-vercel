8e558c0a5453e63c512b506c39bda2439087b77d
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-27 12:54:04 +0630

    config prod

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@
 module.exports = {
   googleClientID:
     "967569194656-2gganjqb136ha2rrrc776u4m6a62f6qd.apps.googleusercontent.com",
-  googleClientSecret: "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",
+  googleClientSecret: "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",
   mongoURI:
     "mongodb+srv://netninja:Y8HxPeseo98w27n4@nodetuts.okytrdd.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "dfjoeioreoref;eifo",
