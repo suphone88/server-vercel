2ef7a94a83009fbbb1fc6d2a237bb76297e2486a
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-27 11:06:50 +0630

    prod with config

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    "340703020399-95bljojurh1sd83tq5d51dvfa26p05ou.apps.googleusercontent.com",
-  googleClientSecret: "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",
+    "967569194656-2gganjqb136ha2rrrc776u4m6a62f6qd.apps.googleusercontent.com",
+  googleClientSecret: "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",
   mongoURI:
     "mongodb+srv://netninja:Y8HxPeseo98w27n4@nodetuts.okytrdd.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "dfjoeioreoref;eifo",
