b20b6b6dbc1afe66b90e71b8a0de465dd41ed9d6
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-27 09:34:00 +0630

    change config

--- config/keyslocal.js
+++ config/keyslocal.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    "340703020399-95bljojurh1sd83tq5d51dvfa26p05ou.apps.googleusercontent.com",
-  googleClientSecret: "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",
+    "711847719529-8un9f55ovtm23nrqoleknd226jakr3sg.apps.googleusercontent.com",
+  googleClientSecret: "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",
   mongoURI:
     "mongodb+srv://netninja:Y8HxPeseo98w27n4@nodetuts.okytrdd.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "dfjoeioreoref;eifo",
