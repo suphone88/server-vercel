e91c88c269795d660e9eaba940b709c9e8b4a9d5
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-27 12:42:48 +0630

    change config

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,9 @@
 // prod.js -
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
+  googleClientID:
+    "967569194656-2gganjqb136ha2rrrc776u4m6a62f6qd.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI:
+    "mongodb+srv://netninja:Y8HxPeseo98w27n4@nodetuts.okytrdd.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "dfjoeioreoref;eifo",
 };
