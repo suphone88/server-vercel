5be300d6212233edf7aff34616c1505b65b988ba
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-27 09:29:48 +0630

    add current user

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require("../config/keys.js");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,7 +23,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
