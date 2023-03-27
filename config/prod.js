2a312fbc8790b377ce369f41da4e70c54959f71c
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-27 13:43:12 +0630

    update config and git

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@
 module.exports = {
   googleClientID:
     "967569194656-2gganjqb136ha2rrrc776u4m6a62f6qd.apps.googleusercontent.com",
-  googleClientSecret: "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",
+  googleClientSecret: "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",
   mongoURI:
     "mongodb+srv://netninja:Y8HxPeseo98w27n4@nodetuts.okytrdd.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "dfjoeioreoref;eifo",
