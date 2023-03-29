7e13633cffc40ea2a81b6b5e87b0d1b2a94cc502
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-29 09:34:38 +0630

    update link setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,11 +3,7 @@ module.exports = function (app) {
   app.use(
     ["/api", "/auth/google"],
     createProxyMiddleware({
-<<<<<<< HEAD
-      target: "http://localhost:5000",
-=======
       target: "https://server-vercel-2whz.vercel.app",
->>>>>>> 8c28640ce248532bb61c6aff95808621856ff648
     })
   );
 };
