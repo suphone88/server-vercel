4cfcea2a6bb87e6d2691c3745bc00bc76401a6ab
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-31 10:53:51 +0630

    add code setupProxy

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,11 @@
-//import axios from "axios";
-import api from "../api/api";
+import axios from "axios";
+//import api from "../api/api";
 
 import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
-  //const res = await axios.get("/api/current_user");
-  const res = await api.get("/api/current_user");
+  const res = await axios.get("/api/current_user");
+  //const res = await api.get("/api/current_user");
   console.log("Actions Auth >>", res);
   dispatch({ type: FETCH_USER, payload: res.data });
 };
