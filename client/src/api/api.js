b87771844cdd8d46f76ee3e79a1dcad2d8b08596
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-31 10:15:31 +0630

    add api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
   //baseURL: "http://localhost:5000",
 
-  baseURL: "https://node-js-test-api-nine.vercel.app",
+  baseURL: "https://node-js-test-api-nine.vercel.app/auth/google",
 });
