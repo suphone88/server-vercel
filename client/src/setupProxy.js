84c76558c824bc86b381ca48c15da298831ed995
Dev Su Su <suphonemyint.la@gmail.com>
2023-04-05 13:56:01 +0630

    change link setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,9 +5,9 @@ module.exports = function (app) {
   app.use(
     ["/api", "/auth/google"],
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      //target: "http://localhost:5000",
 
-      //target: "https://server-vercel-2whz.vercel.app",
+      target: "https://server-vercel-2whz.vercel.app",
       // pathRewrite: {
       //   "^/api": "", // strip "/api" from the URL
       //   "^/auth/google": "",
